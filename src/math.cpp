823cf09b450ab1ce7287bbf5894352a7d18cc59a
Ghoul <CursedGhoulBlox@gmail.com>
2026-05-09 22:26:35 -0400

    Create math.cpp

---
+++ src/math.cpp
@@ -0,0 +1,24 @@
+#include "math.hpp"
+#include <iostream>
+
+namespace astd {
+	double power(double num1, double num2) {
+		double powerResult{ num1 };
+
+		for (int i{ 0 }; i < num2; i += 2) {
+			powerResult = num1 * powerResult;
+		}
+
+		return powerResult;
+	}
+
+	int power(int num1, int num2) {
+		int powerResult{ num1 };
+
+		for (int i{ 0 }; i < num2; i += 2) {
+			powerResult = num1 * powerResult;
+		}
+
+		return powerResult;
+	}
+}
