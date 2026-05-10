b40b99607755d4f01f09434dc4f411e0b7092e76
Ghoul <CursedGhoulBlox@gmail.com>
2026-05-10 01:36:38 -0400

    Create time.cpp

---
+++ src/time.cpp
@@ -0,0 +1,23 @@
+#include <chrono>
+
+namespace astd {
+	auto getSystemTime(bool local = false) {
+		if (local) {
+			return std::chrono::current_zone()->to_local(std::chrono::system_clock::now());
+		}
+
+		else {
+			return std::chrono::system_clock::now();
+		}
+	}
+
+	auto getSystemTrimmedTime(bool local = false) {
+		if (local) {
+			return std::chrono::current_zone()->to_local(std::chrono::time_point_cast<std::chrono::seconds>(std::chrono::system_clock::now()));
+		}
+
+		else {
+			return std::chrono::time_point_cast<std::chrono::seconds>(std::chrono::system_clock::now());
+		}
+	}
+}
