a3fc856d0a3f99e6ae5cec6b33cc2df58b651ea3
Ghoul <CursedGhoulBlox@gmail.com>
2026-05-12 12:30:07 -0400

    Update time.hpp

--- src/time/time.hpp
+++ src/time/time.hpp
@@ -6,4 +6,4 @@ namespace astd {
   auto getSystemTrimmedTime(bool local = false);
 }
 
-// there is a special one for trimmed and non trimmed as trimming changes return types (probably a better way to do it like making it a string but I'm too tired for that and want it to retain usefulness as the time point datatype.)
+// there is a special one for trimmed and non trimmed as trimming changes return types (probably a better way to do it like making it a string but I want it to retain usefulness as the time point datatype.)
