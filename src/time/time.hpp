b62c86ece9c1e241d0f2c7493fb3b408fb577c69
Ghoul <CursedGhoulBlox@gmail.com>
2026-05-12 12:29:42 -0400

    fix part 2 electric boogaloo

--- src/time/time.hpp
+++ src/time/time.hpp
@@ -2,8 +2,8 @@
 
 #include <chrono>
 namespace astd {
-  auto getSystemTime();
-  auto getSystemTrimmedTime();
+  auto getSystemTime(bool local = false);
+  auto getSystemTrimmedTime(bool local = false);
 }
 
 // there is a special one for trimmed and non trimmed as trimming changes return types (probably a better way to do it like making it a string but I'm too tired for that and want it to retain usefulness as the time point datatype.)
