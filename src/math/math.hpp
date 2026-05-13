0911e64115da33cc5f82ea9c0b38ec76a84194a4
Ghoul <CursedGhoulBlox@gmail.com>
2026-05-12 20:59:00 -0400

    better

--- src/math/math.hpp
+++ src/math/math.hpp
@@ -1,18 +1,29 @@
-template<typename T, typename U>
-	auto power(T base, U exponent) {
-		auto powerResult{ base };
-		if (exponent <= 0) {
-			return 1;
-		}
+auto power(auto base, auto exponent) {
+	auto powerResult{ base };
 
-		for (int i{ 0 }; i < exponent; ++i) {
-			powerResult *= base;
-		}
-
-		return powerResult;
+	if (exponent <= 0) {
+		return 1;
 	}
 
-template<typename W>
-auto square(W base) {
+	for (int i{ 0 }; i < exponent; ++i) {
+		powerResult *= base;
+	}
+
+	return powerResult;
+}
+
+auto square(auto base) {
 	return base * base;
+}
+
+bool getNumType(auto num) {
+	// True for even, false for odd
+
+	if (num % 2 == 0) {
+		return true;
+	}
+
+	else {
+		return false;
+	}
 }
