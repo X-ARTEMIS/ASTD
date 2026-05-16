9ec022a61ab2e6c163ef8c45e559cd1ad36cb52a
Ghoul <CursedGhoulBlox@gmail.com>
2026-05-16 00:20:25 -0400

    constified

--- src/time/time.hpp
+++ src/time/time.hpp
@@ -2,8 +2,8 @@
 
 #include <chrono>
 namespace astd {
-  auto getSystemTime(bool local = false);
-  auto getSystemTrimmedTime(bool local = false);
+  auto getSystemTime(const bool local = false);
+  auto getSystemTrimmedTime(const bool local = false);
 }
 
 // there is a special one for trimmed and non trimmed as trimming changes return types (probably a better way to do it like making it a string but I want it to retain usefulness as the time point datatype.)
