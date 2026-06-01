79e4364895c506ad70c007d3746ffc22ee755225
Ghoul <CursedGhoulBlox@gmail.com>
2026-06-01 15:31:30 -0400

    ned moer overloads

--- astd/math/fraction.hpp
+++ astd/math/fraction.hpp
@@ -28,15 +28,73 @@ public:
 
 		return *this;
 	}
+
+	fraction& operator-=(fraction& fraction2) {
+		if (!(this->denominator == fraction2.denominator)) {
+			double lcm = std::lcm(this->denominator, fraction2.denominator);
+
+			this->numerator = (lcm / this->denominator) * this->numerator -
+				(lcm / fraction2.denominator) * fraction2.numerator;
+			this->denominator = lcm;
+		}
+
+		else {
+			this->numerator -= fraction2.numerator;
+		}
+
+		return *this;
+	}
+
+	fraction& operator*=(fraction& fraction2) {
+		if (!(this->denominator == fraction2.denominator)) {
+			double lcm = std::lcm(this->denominator, fraction2.denominator);
+
+			this->numerator = (lcm / this->denominator) * this->numerator *
+				(lcm / fraction2.denominator) * fraction2.numerator;
+			this->denominator = lcm;
+		}
+
+		else {
+			this->numerator *= fraction2.numerator;
+		}
+
+		return *this;
+	}
+
+	fraction& operator/=(fraction& fraction2) {
+		if (!(this->denominator == fraction2.denominator)) {
+			double lcm = std::lcm(this->denominator, fraction2.denominator);
+
+			this->numerator = (lcm / this->denominator) * this->numerator /
+				(lcm / fraction2.denominator) * fraction2.numerator;
+			this->denominator = lcm;
+		}
+
+		else {
+			this->numerator /= fraction2.numerator;
+		}
+
+		return *this;
+	}
 };
 
-template<typename T, typename U>
-fraction<T, U>& operator+(fraction<T, U>& frac, fraction<T, U> frac2) {
+template<typename A, typename B, typename C, typename D, typename E, typename F> // I doubt this is necessary
+fraction<A, B>& operator+(fraction<C, D>& frac, fraction<E, F> frac2) {
 	return frac += frac2;
 }
 
-template<typename T, typename U>
-std::ostream& operator<<(std::ostream& out, fraction<T, U> frac) {
+template <typename G, typename H, typename I, typename J, typename K, typename L>
+fraction<G, H>& operator-(fraction<I, J>& frac, fraction<K, L> frac2) {
+	return frac -= frac2;
+}
+
+template<typename M, typename N, typename O, typename P, typename Q, typename R>
+fraction<M, N>& operator*(fraction<O, P> frac, fraction<Q, R> frac2) {
+	return frac *= frac2;
+}
+
+template<typename S, typename V>
+std::ostream& operator<<(std::ostream& out, fraction<S, V> frac) {
 	out << frac.numerator << '/' << frac.denominator;
 	return out;
 }
