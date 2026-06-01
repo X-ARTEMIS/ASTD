ad618730198ac272693cfef3f904f9e9b6eb43f9
Ghoul <CursedGhoulBlox@gmail.com>
2026-06-01 15:15:25 -0400

    operator+ overloads

--- astd/math/fraction.hpp
+++ astd/math/fraction.hpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <numeric>
 
 template<typename T, typename U>
 class fraction {
@@ -6,15 +7,36 @@ public:
 	T numerator{};
 	U denominator{};
 
-	fraction(T numerator, U denominator) : numerator{ numerator }, denominator{ denominator } { if (denominator == 0) { throw "Undefined fraction"; } }
+	fraction(T numerator, U denominator) : numerator{ numerator }, denominator{ denominator } { if (denominator == 0) { throw std::exception("Undefined fraction"); } }
 
 	double fractionToDecimal() {
 		return static_cast<double>(this->numerator) / static_cast<double>(this->denominator);
 	}
+
+	fraction& operator+=(fraction& fraction2) {
+		if (!(this->denominator == fraction2.denominator)) { 
+			double lcm = std::lcm(this->denominator, fraction2.denominator);
+
+			this->numerator = (lcm / this->denominator) * this->numerator +
+				(lcm / fraction2.denominator) * fraction2.numerator;
+			this->denominator = lcm;
+		}
+
+		else {
+			this->numerator += fraction2.numerator;
+		}
+
+		return *this;
+	}
 };
 
 template<typename T, typename U>
+fraction<T, U>& operator+(fraction<T, U>& frac, fraction<T, U> frac2) {
+	return frac += frac2;
+}
+
+template<typename T, typename U>
 std::ostream& operator<<(std::ostream& out, fraction<T, U> frac) {
-	out << frac.numerator << "/" << frac.denominator;
+	out << frac.numerator << '/' << frac.denominator;
 	return out;
 }
